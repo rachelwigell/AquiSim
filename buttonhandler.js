f874f74da088d7077bf71c6799c76a754dcd6567
rachelwigell <rachel.wigell@wpi.edu>
2015-08-25 22:12:50 -0400

    troubleshooting

--- buttonhandler.js
+++ buttonhandler.js
@@ -1,6 +1,7 @@
 var message = "Hello from Processing!"
 
 $('#test_button').click(function() {
+	alert("Well, the button does something at least.")
 	message = "Hello from Javascript!" 
 	$('#messages').text(message);
 });
